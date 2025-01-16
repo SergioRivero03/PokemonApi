01402b3510031d29ef6e5ee3984a8e526207d3a3
Sergio <sergioriveroblanco03@gmail.com>
2025-01-16 15:44:44 +0100

    Primer commit

---
+++ PokemonApi/src/test/java/dis/ufv/RestApi/PokemonApi/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dis.ufv.RestApi.PokemonApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
